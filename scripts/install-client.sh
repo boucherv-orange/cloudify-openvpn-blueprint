a05b2797ab12bfe8d5304dcd64a1fd5f0b440c87
Valentin Boucher <valentin.boucher@orange.com>
2017-04-04 10:39:57 +0200

    Update install-client.sh

--- scripts/install-client.sh
+++ scripts/install-client.sh
@@ -8,7 +8,7 @@ wget http://$OPENVPN_SERVER_IP/vpnaccess.tar.bz2
 
 tar -xvf vpnaccess.tar.bz2
 
-sed -i "s/user nobody/user cloud/g" client.conf 
+sed -i "s/user nobody/user ubuntu/g" client.conf 
 
 sudo sysctl -w net.ipv4.ip_forward=1
 
